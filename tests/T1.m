2d0d1593ae7ebc08df461d6e308f90a92bfadb40
apuvvala <apuvvala@VDI-WD1AH2-408>
2024-03-25 14:22:51 -0400

    introduced a failure in T1.m

--- tests/T1.m
+++ tests/T1.m
@@ -2,7 +2,7 @@ classdef T1 < matlab.unittest.TestCase
 
     methods (Test)
         function s_add_doubles(testCase)
-            testCase.verifyEqual(s_add(1,1), 2);
+            testCase.verifyEqual(s_add(1,1), 3);
         end
     end
     
