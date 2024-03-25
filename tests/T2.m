ecf8fd83d55fc360a143b09c86efcd0881326fa5
apuvvala <apuvvala@VDI-WD1AH2-408>
2024-03-25 14:23:19 -0400

    Introduced a failure in T2.m

--- tests/T2.m
+++ tests/T2.m
@@ -2,7 +2,7 @@ classdef T2 < matlab.unittest.TestCase
 
     methods (Test)
         function s_multiply_doubles(testCase)
-            testCase.verifyEqual(s_multiply(1,1), 1);
+            testCase.verifyEqual(s_multiply(1,1), 2);
         end
     end
     
