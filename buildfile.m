997ff3834d92499ba1e4949ed7fd97f80a4eb758
apuvvala <apuvvala@VDI-WD1AH2-408>
2024-04-04 14:33:15 -0400

    Removing unnecessary files

--- buildfile.m
+++ buildfile.m
@@ -5,6 +5,7 @@ import matlab.buildtool.tasks.TestTask
 import matlab.buildtool.tasks.PcodeTask
 import matlab.buildtool.Task
 
+
 plan = buildplan(localfunctions);
 
 origPath = addpath("src");
@@ -14,7 +15,7 @@ plan("clean") = CleanTask;
 plan("check") = CodeIssuesTask;
 
 plan("test") = TestTask("tests", SourceFiles="src", CodeCoverageResults="results/cov.xml", TestResults="results/tr.xml");
-plan("pcode") = PcodeTask("l*.m", pwd);
+% plan("pcode") = PcodeTask("l*.m", pwd);
 % plan("test") = TestTask();
 % plan("test") = TestTask("tests\T1.m", SourceFiles="src");
 
@@ -22,14 +23,5 @@ plan("pcode") = PcodeTask("l*.m", pwd);
 % modifiedFiles = proj.listModifiedFiles();
 % plan("prequalify") = TestTask(SourceFiles=[modifiedFiles.Path]);
 
-plan("cacheSave").Inputs = "results";
-
-plan.DefaultTasks = ["check" "test", "pcode"];
+plan.DefaultTasks = ["check" "test"];
 end
-
-function cacheSaveTask(context)
-outputFolder = context.Task.Inputs.absolutePaths();
-
-cacheFolder = 
-end
-
--- lookUpTestTaskTrace.m
+++
@@ -1,5 +0,0 @@
-repo = matlab.buildtool.internal.fingerprints.PersistentTaskTraceRepository(matlab.buildtool.internal.cacheRoot(pwd));
-t = repo.lookupTrace("test");
-t.Inputs
-t.Outputs
-t.Arguments
