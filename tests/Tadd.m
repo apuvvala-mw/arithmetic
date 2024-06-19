863044dd11af056992e82555e7b43f0440261952
apuvvala <apuvvala@VDI-WD1AH2-408>
2024-06-19 16:18:24 -0400

    Added a data file

--- tests/Tadd.m
+++ tests/Tadd.m
@@ -2,6 +2,7 @@ classdef Tadd < matlab.unittest.TestCase
 
     methods (Test)
         function s_add_doubles(testCase)
+            load("data/myfile.mat");
             testCase.verifyEqual(s_add(1,1), 2);
         end
     end
