e372ab00aa6ff30849d0519eb895981239567450
Ajay Puvvala <115959883+apuvvala-mw@users.noreply.github.com>
2024-09-17 09:19:13 -0400

    Update buildfile.m
    Adding code issues task

--- buildfile.m
+++ buildfile.m
@@ -7,8 +7,7 @@ origPath = addpath("src");
 c = onCleanup(@()path(origPath));
 
 plan("clean") = CleanTask;
-% plan("test") = TestTask(SourceFiles=["src", "tests/data"], CodeCoverageResults="results/cov.html");
-% plan("test") = TestTask(SourceFiles="src", CodeCoverageResults="results/cov.mat");
+plan("check") = CodeIssuesTask(Results="results/issues.sarif");
 plan("test") = TestTask(SourceFiles=["src/s_add.m", "tests/data"], CodeCoverageResults="results/cov.html");
 plan.DefaultTasks = "test";
 end
