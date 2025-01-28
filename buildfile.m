2a216d39564c0cf891ba0532e3b01902ab91d957
Ajay Puvvala <apuvvala@mathworks.com>
2025-01-28 11:37:58 -0500

    run only impacted tests using repo.ModifiedFiles

--- buildfile.m
+++ buildfile.m
@@ -2,17 +2,23 @@ function plan = buildfile
 import matlab.buildtool.tasks.*
 
 plan = buildplan(localfunctions);
+addpath("src");
+repo = matlab.git.GitRepository
+modifiedFiles = repo.ModifiedFiles
+suite = testsuite()
+suite = suite.selectIf(matlabtest.selectors.DependsOn(modifiedFiles))
+testFiles = [suite.Filename]
 
 origPath = addpath("src");
 c = onCleanup(@()path(origPath));
 
 plan("clean") = CleanTask;
 plan("check") = CodeIssuesTask(Results="issues.sarif");
-plan("test") = TestTask(SourceFiles=["src/s_add.m", "tests/data"], CodeCoverageResults="results/cov.html");
+plan("test") = TestTask(testFiles, SourceFiles=["src/s_add.m", "tests/data"], CodeCoverageResults="results/cov.html");
 plan.DefaultTasks = ["modFiles" "check" "test"];
 end
 
 function modFilesTask(ctx)
-repo = matlab.git.GitRepository
+repo = matlab.git.GitRepository;
 disp(repo.ModifiedFiles)
 end
