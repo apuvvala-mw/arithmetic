79c8525c2ca7519e78f5ad11f387aef69ff6fc8a
Ajay Puvvala <apuvvala@mathworks.com>
2025-01-28 11:18:41 -0500

    trying out modified files

--- buildfile.m
+++ buildfile.m
@@ -8,6 +8,11 @@ c = onCleanup(@()path(origPath));
 
 plan("clean") = CleanTask;
 plan("check") = CodeIssuesTask(Results="issues.sarif");
-plan("test") = TestTask(SourceFiles=["src/s_add.m", "tests/data"], CodeCoverageResults="results/cov.html", RunOnlyImpactedTests=true);
-plan.DefaultTasks = ["check" "test"];
+plan("test") = TestTask(SourceFiles=["src/s_add.m", "tests/data"], CodeCoverageResults="results/cov.html");
+plan.DefaultTasks = ["modFiles" "check" "test"];
+end
+
+function modFilesTask(ctx)
+repo = matlab.git.GitRepository;
+disp(repo.ModifiedFiles)
 end
