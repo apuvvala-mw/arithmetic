10a31ba2b83fe01b67166a147ad3bc1848d51ab7
Ajay Puvvala <apuvvala@mathworks.com>
2025-01-29 13:15:02 -0500

    Impacted Since

--- buildfile.m
+++ buildfile.m
@@ -2,23 +2,24 @@ function plan = buildfile
 import matlab.buildtool.tasks.*
 
 plan = buildplan(localfunctions);
-addpath("src");
-repo = matlab.git.GitRepository
-modifiedFiles = repo.ModifiedFiles
-suite = testsuite()
-suite = suite.selectIf(matlabtest.selectors.DependsOn(modifiedFiles))
-testFiles = [suite.Filename]
+% addpath("src");
+% repo = matlab.git.GitRepository
+% modifiedFiles = repo.ModifiedFiles
+% suite = testsuite()
+% suite = suite.selectIf(matlabtest.selectors.DependsOn(modifiedFiles))
+% testFiles = [suite.Filename]
 
 origPath = addpath("src");
 c = onCleanup(@()path(origPath));
 
 plan("clean") = CleanTask;
 plan("check") = CodeIssuesTask(Results="issues.sarif");
-plan("test") = TestTask(testFiles, SourceFiles=["src/s_add.m", "tests/data"], CodeCoverageResults="results/cov.html");
-plan.DefaultTasks = ["modFiles" "check" "test"];
+plan("test") = TestTask(SourceFiles=["src/s_add.m", "tests/data"], CodeCoverageResults="results/cov.html", RunOnlyImpactedTests=true, ImpactedSince="main");
+% plan.DefaultTasks = ["modFiles" "check" "test"];
+plan.DefaultTasks = ["check" "test"];
 end
 
 function modFilesTask(ctx)
 repo = matlab.git.GitRepository;
-disp(repo.ModifiedFiles)
+disp(repo.ModifiedFiles);
 end
